8107fe291fbdf60b08b4ac8e10d297129778c540
Jmartz-01 <jpmartinezci@correo.url.edu.gt>
2026-03-13 23:41:30 -0600

    feat: Ej 1 subido actividad 25

--- ACT 25/EJ1/Program.cs
+++ ACT 25/EJ1/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Registro de Usuarios
+int opcion = 0;
+int edad = 0;
+int tipoNum = 0;
+string nombre = "";
+string correo = "";
+string tipoUsuario = "";
+
+do
+{
+    Console.WriteLine("1. Registrar usuario");
+    Console.WriteLine("2. Mostrar usuario");
+    Console.WriteLine("3. Buscar usuario");
+    Console.WriteLine("4. Salir");
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Nombre:");
+            nombre = Console.ReadLine();
+            if (nombre.Length < 3)
+            {
+                Console.WriteLine("Error: Nombre muy corto");
+            }
+
+            Console.WriteLine("Edad:");
+            edad = int.Parse(Console.ReadLine());
+            if (edad < 18)
+            {
+                Console.WriteLine("Es menor de edad");
+            }
+
+            Console.WriteLine("Correo:");
+            correo = Console.ReadLine();
+            if (!correo.Contains("@"))
+            {
+                Console.WriteLine("Advertencia: Correo no valido");
+            }
+
+            Console.WriteLine("Tipo: 1.Admin, 2.Cliente, 3.Invitado");
+            tipoNum = int.Parse(Console.ReadLine());
+            switch (tipoNum)
+            {
+                case 1: tipoUsuario = "Administrador"; break;
+                case 2: tipoUsuario = "Cliente"; break;
+                case 3: tipoUsuario = "Invitado"; break;
+            }
+            break;
+
+        case 2:
+            Console.WriteLine("Usuario: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Correo: " + correo);
+            Console.WriteLine("Tipo: " + tipoUsuario);
+            break;
+
+        case 3:
+            Console.WriteLine("Ingrese nombre a buscar:");
+            string buscar = Console.ReadLine();
+            if (buscar == nombre)
+            {
+                Console.WriteLine("Usuario encontrado: " + nombre);
+            }
+            else
+            {
+                Console.WriteLine("No encontrado");
+            }
+            break;
+    }
+} while (opcion != 4);
